7ac7e7dcc93fb99d13b983a5c463c8dc5c206fd7
Savio Resende <savio@savioresende.com>
2022-07-02 21:49:17 -0600

    wip

--- config/app.php
+++ config/app.php
@@ -3,6 +3,6 @@
 return [
     'env' => env('APP_ENV'),
     'app-name' => env('APP_NAME', 'Kanata'),
-    'app-url' => env('APP_URL', null),
+    'app-url' => env('APP_URL', '127.0.0.1') . ':' . env('APP_PORT', '8080'),
     'session-key' => env('SESSION_PREFIX', 'eyJpZCI6IjU0'),
 ];
